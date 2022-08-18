bddd4fb9eb196e27dde0d575c4374f93a993ff20
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-08-18 16:14:15 +0300

    update

--- install.sh
+++ install.sh
@@ -11,18 +11,21 @@ sleep 5 && docker ps -a || true
            
 terraform init && terraform plan
 terraform apply -auto-approve
-sleep 10 && kubectl get pods -A
+sleep 10 && kubectl get pods -A && sleep 5
 
              echo      "----- ............................. -----"
              echo         "---  LOAD-ARGO-APPLICATIONS  ---"
              echo      "----- ............................. -----"
-             printf "\nWaiting for the echo web server service... \n"
-             
+             printf "\nWaiting for application will be ready... \n"
+sleep 5             
 kubectl apply -f ./${path_folder}/app-apache.yaml
 kubectl apply -f ./${path_folder}/app-httpd.yaml
 kubectl apply -f ./${path_folder}/ingress-argocd.yaml
-sleep 10
-
+sleep 5
+             printf "\nWaiting for application will be ready... \n"
 printf "\nYou should see 'foo' as a reponse below (if you do the ingress is working):\n"
 kubectl get nodes -o wide && sleep 5
 terraform providers
+             echo      "----- ............................. -----"
+             echo           "---  APPLICATIONS READY  ---"
+             echo      "----- ............................. -----"
