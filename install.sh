ca29838e453d06f49dfe8cba75fdeb2c53cf4e92
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-08-18 16:16:55 +0300

    update

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sleep 5 && docker ps -a || true
              echo      "----- ............................. -----"
              echo         "---  LOAD-CONFIG-TERRAFORM  ---"
              echo      "----- ............................. -----"
-           
+sleep 5         
 terraform init && terraform plan
 terraform apply -auto-approve
 sleep 10 && kubectl get pods -A && sleep 5
@@ -16,7 +16,7 @@ sleep 10 && kubectl get pods -A && sleep 5
              echo      "----- ............................. -----"
              echo         "---  LOAD-ARGO-APPLICATIONS  ---"
              echo      "----- ............................. -----"
-             printf "\nWaiting for application will be ready... \n"
+             
 sleep 5             
 kubectl apply -f ./${path_folder}/app-apache.yaml
 kubectl apply -f ./${path_folder}/app-httpd.yaml
@@ -27,5 +27,5 @@ printf "\nYou should see 'foo' as a reponse below (if you do the ingress is work
 kubectl get nodes -o wide && sleep 5
 terraform providers
              echo      "----- ............................. -----"
-             echo           "---  APPLICATIONS READY  ---"
+             echo           "---  CLUSTER IS READY  ---"
              echo      "----- ............................. -----"
