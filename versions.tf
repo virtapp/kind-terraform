5843a23513aa5849d65a20f2d31a77d14156c48b
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-08-20 19:35:36 +0300

    update

--- versions.tf
+++ versions.tf
@@ -3,17 +3,17 @@ terraform {
   required_providers {
     kind = {
       source  = "kyma-incubator/kind"
-      version = "0.0.9"
+      version = "0.0.11"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.5.0"
+      version = "2.9.0"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = "2.3.0"
+      version = "2.4.1"
     }
 
     null = {
