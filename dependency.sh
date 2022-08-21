66eba7d854fce9b3c5eea2d0a10d559378883b4e
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-08-22 01:13:11 +0300

    update

--- dependency.sh
+++ dependency.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt-get update -y && apt-get install docker.io -y
+sleep 5
    echo    "----- ..................................................... -----"
    echo      "----- ...............   TERRAFORM .................... -----"
    echo    "----- ..................................................... -----"
@@ -10,7 +11,7 @@ sudo unzip terraform_0.14.3_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 sleep 5
    echo    "----- ..................................................... -----"
-   echo        "----- ...............   HELM .................... -----"
+   echo        "----- ...............   HELM   .................... -----"
    echo    "----- ..................................................... -----"
 curl -# -LO https://get.helm.sh/helm-v3.5.3-linux-amd64.tar.gz
 tar -xzvf helm-v3.5.3-linux-amd64.tar.gz
@@ -18,7 +19,7 @@ mv linux-amd64/helm /usr/local/bin/helm
 rm -rf helm-v3.5.3-linux-amd64.tar.gz && rm -rf linux-amd64
 sleep 5
    echo    "----- ..................................................... -----"
-   echo       "----- ...............   KUBECTL .................... -----"
+   echo       "----- ...............   KUBECTL  .................... -----"
    echo    "----- ..................................................... -----"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
