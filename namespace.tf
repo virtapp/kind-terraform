ad40116bfa80220e04698979af6759af5afaf240
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-08-23 18:08:26 +0300

    update

---
+++ namespace.tf
@@ -0,0 +1,8 @@
+# Creating namespace with the Kubernetes provider is better than auto-creation in the helm_release.
+# You can reuse the namespace and customize it with quotas and labels.
+resource "kubernetes_namespace" "centerity" {
+  metadata {
+    name = var.namespace
+  }
+}
+
