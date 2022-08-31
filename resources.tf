71f81b0daa0e21c26ed04ca5da6f24e999274e03
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-08-31 16:53:35 +0300

    update

--- resources.tf
+++ resources.tf
@@ -27,3 +27,20 @@ resource "helm_release" "grafana" {
     value = "ClusterIP"
   }
 }
+
+###-devtron
+resource "helm_release" "devtron" {
+  name       = "devtron"
+  repository = "https://helm.devtron.ai"
+  chart      = "devtron-operator"
+  version    = var.devtron_helm_version
+
+  namespace        = var.devtron_namespace
+  create_namespace = true
+
+  values = [file("devtron.yaml")]
+
+  timeout = 600000
+
+  depends_on = [kind_cluster.default]
+}
