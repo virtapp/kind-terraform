18bc0c6c9988531cee7b86a72887ef6d31bed2da
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-09-12 12:10:52 +0300

    update

--- resources.tf
+++ resources.tf
@@ -27,3 +27,31 @@ resource "helm_release" "grafana" {
     value = "ClusterIP"
   }
 }
+
+###-rancher
+resource "helm_release" "rancher_server" {
+  depends_on = [
+    kind_cluster.default,
+  ]
+
+  name             = "rancher"
+  chart            = "https://releases.rancher.com/server-charts/latest/rancher-${var.rancher_version}.tgz"
+  namespace        = "cattle-system"
+  create_namespace = true
+  wait             = true
+
+  set {
+    name  = "hostname"
+    value = var.rancher_server_dns
+  }
+
+  set {
+    name  = "replicas"
+    value = "1"
+  }
+
+  set {
+    name  = "bootstrapPassword"
+    value = "admin" # TODO: change this once the terraform provider has been updated with the new pw bootstrap logic
+  }
+}
