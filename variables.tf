967c6391f9ddc4a45a4244d1bbc8cb857693f894
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-09-12 17:04:29 +0300

    update

--- variables.tf
+++ variables.tf
@@ -24,6 +24,7 @@ variable "ingress_nginx_namespace" {
   default     = "ingress-nginx"
 }
 
+# Required
 variable "rancher_version" {
   type        = string
   description = "Rancher server version (format v0.0.0)"
@@ -35,4 +36,11 @@ variable "rancher_server_dns" {
   type        = string
   description = "DNS host name of the Rancher server"
   default     = "console.centerity.com"
+}
+
+# Required
+variable "admin_password" {
+  type        = string
+  description = "Admin password to use for Rancher server bootstrap, min. 12 characters"
+  default     = "q1w2e3r4100@"
 }
