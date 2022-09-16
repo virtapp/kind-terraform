c2ee80d9e58796804596f2f2c6f3a34e5a169571
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-09-16 23:39:22 +0300

    update

--- argocd.tf
+++ argocd.tf
@@ -13,3 +13,21 @@ resource "helm_release" "argocd" {
   ]
   depends_on = [kind_cluster.default]
 }
+
+resource "null_resource" "wait_for_argocd" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\nWaiting for the argocd controller...\n"
+      kubectl wait --namespace ${helm_release.argocd.namespace} \
+        --for=condition=ready pod \
+        --selector=app.kubernetes.io/component=server \
+        --timeout=90s
+    EOF
+  }
+
+  depends_on = [helm_release.argocd]
+}
