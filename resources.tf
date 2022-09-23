b5782c3c86dee701626d43823d99aa5f9b8ca2fe
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-09-24 01:26:20 +0300

    update

--- resources.tf
+++ resources.tf
@@ -5,7 +5,7 @@ resource "helm_release" "keycloak" {
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "keycloak"
   create_namespace = true
-  depends_on = [helm_release.argocd,kind_cluster.default]]
+  depends_on = [helm_release.argocd,kind_cluster.default]
 
   set {
     name  = "service.type"
