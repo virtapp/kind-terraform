9db9f3ada5126592490a79e82ad2d8bd86ec00fa
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-09-23 16:03:36 +0300

    update

--- resources.tf
+++ resources.tf
@@ -30,38 +30,6 @@ resource "helm_release" "grafana" {
 }
 
 
-###-rancher
-resource "helm_release" "rancher_server" {
-
-  name             = "rancher"
-  chart            = "https://releases.rancher.com/server-charts/latest/rancher-${var.rancher_version}.tgz"
-  namespace        = "cattle-system"
-  create_namespace = true
-  wait             = true
-  depends_on = [kind_cluster.default]
-
-  set {
-    name  = "hostname"
-    value = var.rancher_server_dns
-  }
-
-  set {
-    name  = "ingress.tls.source"
-    value = "centerity"
-  }
-
-  set {
-    name  = "replicas"
-    value = "1"
-  }
-
-  set {
-    name  = "bootstrapPassword"
-    value = "admin" # TODO: change this once the terraform provider has been updated with the new pw bootstrap logic
-  }
-}
-
-
 ###-keda
 resource "helm_release" "keda" {
   name = "keda"
