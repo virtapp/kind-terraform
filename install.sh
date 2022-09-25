b27109cb5e3178fa331ec1857ac07bbceeb9869d
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-09-26 01:48:33 +0300

    update

--- install.sh
+++ install.sh
@@ -31,10 +31,8 @@ sleep 10 && kubectl get pods -A && sleep 5
              
 helm repo add bitnami https://charts.bitnami.com/bitnami || true
 helm repo add hashicorp https://helm.releases.hashicorp.com|| true
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest || true
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || true
 helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts || true
-helm repo add kedacore https://kedacore.github.io/charts || true
 helm repo update && sleep 5
 helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver && sleep 5
          echo    Waiting for all pods in running mode:
@@ -48,7 +46,7 @@ done  2>/dev/null
 sleep 5 &&           
 kubectl apply -f ./${path_folder}/app-apache.yaml
 kubectl apply -f ./${path_folder}/app-httpd.yaml
-sleep 5 && kubectl create namespace centerity || true
+sleep 5 && kubectl create namespace monitor || true
 
                printf "\nWaiting for application will be ready... \n"
 printf "\nYou should see 'dashboard' as a reponse below (if you do the ingress is working):\n"
@@ -61,7 +59,7 @@ kubectl apply -f ./${path_folder}/ingress-keyclock.yaml
 kubectl apply -f ./${path_folder}/ingress-argocd.yaml
 sleep 5 && 
 kubectl get nodes -o wide && sleep 5
-terraform providers
+terraform state list && terraform providers
 
              echo      "----- ............................. -----"
              echo           "---  CLUSTER IS READY  ---"
