a7ec425a26d64803fde848e2a2b9461f469284cc
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-09-26 01:42:26 +0300

    update

--- install.sh
+++ install.sh
@@ -36,19 +36,9 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts || true
 helm repo add kedacore https://kedacore.github.io/charts || true
 helm repo update && sleep 5
-helm fetch rancher-latest/rancher --version=v2.6.2 || true
-kubectl create namespace cattle-system || true
-helm install rancher rancher-latest/rancher --version=v2.6.2 \
-  --namespace cattle-system \
-  --set hostname=console.appflex.io \
-  --set ingress.tls.source=appflex \
-  --set replicas=1 \
-  --set bootstrapPassword="admin"
-kubectl create namespace keda || true
-helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver || true
-helm install keda kedacore/keda --namespace keda && sleep 5
-echo    Waiting for all pods in running mode:
-until kubectl wait --for=condition=Ready pods --all -n keda; do
+helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver && sleep 5
+         echo    Waiting for all pods in running mode:
+until kubectl wait --for=condition=Ready pods --all -n vault; do
 sleep 2
 done  2>/dev/null
 
@@ -59,8 +49,7 @@ sleep 5 &&
 kubectl apply -f ./${path_folder}/app-apache.yaml
 kubectl apply -f ./${path_folder}/app-httpd.yaml
 sleep 5 && kubectl create namespace centerity || true
-#kubectl apply -f ./${path_folder}/secret.yaml || true
-#kubectl apply -f ./${path_folder}/infra.yaml || true
+
                printf "\nWaiting for application will be ready... \n"
 printf "\nYou should see 'dashboard' as a reponse below (if you do the ingress is working):\n"
 
