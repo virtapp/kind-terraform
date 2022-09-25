a03b631730d762fce00a6ad9d4f7ccab9026bebf
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-09-26 00:21:37 +0300

    update

--- resources.tf
+++ resources.tf
@@ -29,30 +29,3 @@ resource "helm_release" "grafana" {
   }
 }
 
-
-###-keda
-resource "helm_release" "keda" {
-  name = "keda"
-
-  repository = "https://kedacore.github.io/charts"
-  chart      = "keda"
-  namespace  = "keda"
-  atomic     = true
-  create_namespace = true
-
-  depends_on = [helm_release.argocd,kind_cluster.default]
-}
-
-
-###-vault
-resource "helm_release" "vault" {
-  chart            = "vault"
-  name             = "vault"
-  namespace        = "vault"
-  create_namespace = true
-  repository       = "https://helm.releases.hashicorp.com"
-  depends_on = [helm_release.argocd,kind_cluster.default]
-  values = [
-    file("argocd/vault-values.yaml")
-  ]
-}
