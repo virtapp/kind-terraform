f40de89566f7084cf236625649821141b7df6948
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2022-09-25 14:04:16 +0300

    update

---
+++ argocd/backend.tf
@@ -0,0 +1,42 @@
+###-backend
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "backend" {
+  bucket_prefix = "local-cluster"
+
+  versioning {
+    enabled = false
+  }
+
+  force_destroy = true
+
+  // lifecycle {
+  //   prevent_destroy = true
+  // }
+}
+
+resource "aws_dynamodb_table" "backend_lock" {
+  name           = "local-cluster"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
+
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.backend.id
+}
+
+output "dynamodb_endpoint" {
+  value = aws_dynamodb_table.backend_lock.name
+}
+
