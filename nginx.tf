2ba81eb613d30f181894e2d2771473db2f70d2c0
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2023-02-11 01:00:37 +0200

    update

--- nginx.tf
+++ nginx.tf
@@ -16,7 +16,7 @@ resource "helm_release" "ingress_nginx" {
   create_namespace = true
   timeout = 120
 
-  values = [file("argocd/nginx_ingress_values.yaml")]
+  values = [file("argocd/nginx_values.yaml")]
 
   depends_on = [kind_cluster.default]
 }
