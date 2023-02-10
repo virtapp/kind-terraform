65c32b3602524cea4822f7c01bb94f2eda61b25e
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2023-02-10 23:10:56 +0200

    update

--- resources.tf
+++ resources.tf
@@ -13,4 +13,19 @@ resource "helm_release" "keycloak" {
   }
 }
 
+#deploy cert manager
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "1.7.1"
+  namespace        = "cert-manager"
+  create_namespace = true
+  #values = [file("cert-manager-values.yaml")]
+  depends_on = [helm_release.argocd,kind_cluster.default]
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
 
+}
