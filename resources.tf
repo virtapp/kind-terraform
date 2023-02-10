5f83f5a79c37f72ec10af70aae672da0dce9cc5a
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2023-02-10 23:07:54 +0200

    update

--- resources.tf
+++ resources.tf
@@ -14,18 +14,3 @@ resource "helm_release" "keycloak" {
 }
 
 
-###-grafana
-resource "helm_release" "grafana" {
-  name       = "grafana"
-
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "grafana"
-  create_namespace = true
-  depends_on = [helm_release.argocd,kind_cluster.default]
-
-  set {
-    name  = "service.type"
-    value = "ClusterIP"
-  }
-}
-
