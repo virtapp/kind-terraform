fef67cfc4e09b635e8f3bb368a2bdfc6074724f4
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2023-02-12 22:37:46 +0200

    update

--- install.sh
+++ install.sh
@@ -60,7 +60,6 @@ sleep 5 &&
 kubectl apply -f ./${path_folder}/app-apache.yaml
 kubectl apply -f ./${path_folder}/app-httpd.yaml
 sleep 5 && kubectl create namespace appflex || true
-kubectl apply -f ./${path_folder}/app-infra.yaml || true
                printf "\nWaiting for application will be ready... \n"
 printf "\nYou should see 'dashboard' as a reponse below (if you do the ingress is working):\n"
 
